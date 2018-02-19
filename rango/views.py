fc0a2a1c64c3bc06e3ae766d395dcb19708bf996
fahad <fahadsiddiqui707@gmail.com>
2018-02-19 23:04:11 +0530

    My Django first commit

---
+++ rango/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	return render(request, 'rango/index.html', context_dict)
+    #return HttpResponse("Rango says hey there world! fahad")
