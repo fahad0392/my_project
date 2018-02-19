aff8d8b3e47c3528309055b7559e2eef868dac15
fahad <fahadsiddiqui707@gmail.com>
2018-02-20 03:04:31 +0530

    view message display-final

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 
 def index(request):
-	context_dict = {'boldmessage': "DESPLAYING BOLD MESSAGE"}
+	context_dict = {'boldmessage': "I am bold font from the contexts"}
 	return render(request, 'rango/index.html', context_dict)
     #return HttpResponse("Rango says hey there world! fahad")
