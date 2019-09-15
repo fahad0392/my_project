9da1a27184ac4cb936c21c6a44e3748aa3a969ea
Md Fahad Iqbal. Siddiqui <RLE0392@rlpl.com>
2019-09-15 13:52:05 +0530

    deleted unwanted

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 def index(request):
 	context_dict = {'boldmessage': "BOLD MESSAGE"}
 	return render(request, 'rango/index.html', context_dict)
-    #return HttpResponse("Rango says hey there world! fahad")
+    
